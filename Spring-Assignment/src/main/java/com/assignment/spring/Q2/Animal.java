776576b8287879df39a7e679d733e310dfd01882
fajju <fajju@Panfap>
2022-04-13 14:05:35 +0530

    Submitting for the first time

---
+++ Spring-Assignment/src/main/java/com/assignment/spring/Q2/Animal.java
@@ -0,0 +1,23 @@
+package com.assignment.spring.Q2;
+
+public class Animal {
+	private String name;
+	private int age;
+	
+	public Animal() {}
+	public Animal(String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+	// Getters and Setters
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+	public int getAge() {return age;}
+	public void setAge(int age) {this.age = age;}
+	
+	public String getDetails() {
+		return name+" is a "+age+" years old animal";
+	}
+	
+	
+}
